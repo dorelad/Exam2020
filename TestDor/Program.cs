5afd3a3cdf59068fcae8c0831c4ad23065a59def
dor <dorelad94@gmail.com>
2020-08-05 12:03:29 +0300

    nana

--- TestDor/Program.cs
+++ TestDor/Program.cs
@@ -6,7 +6,7 @@ namespace TestDor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("nana");
         }
     }
 }

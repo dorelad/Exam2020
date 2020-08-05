4b948b9182a34fccd0c99b3b6e460fe4eecfc398
dor <dorelad94@gmail.com>
2020-08-05 12:38:16 +0300

    add train

---
+++ TestDor/TrainStation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDor
+{
+    class TrainStation
+    {
+        string _city;
+        
+        public TrainStation()
+        {
+            _city = "Tivon";
+        }
+    }
+}
